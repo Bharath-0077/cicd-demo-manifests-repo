2026684c7d6805bdc988953f0b8c0975e9bf5ec3
Bharath-0077 <94161938+Bharath-0077@users.noreply.github.com>
2023-01-10 15:59:03 +0530

    Update getCurrentVersion.sh

--- demo/getCurrentVersion.sh
+++ demo/getCurrentVersion.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-IMAGE_TAG=\$(kubectl get deployment todo-app -n demo -o jsonpath='{\$.spec.template.spec.containers[0].image}')
+IMAGE_TAG=$(kubectl get deployment todo-app -n demo -o jsonpath='{$.spec.template.spec.containers[0].image}')
 IFS=':'; arrIN=${IMAGE_TAG}; unset IFS;
 echo Current Image Version: ${arrIN[1]}
 export CURRENT_IMAGE_VERSION=${arrIN[1]}
